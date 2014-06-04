8d733b2eced503c4a278fb35ff445cbb8cbeb0b3
hc <835965147@qq.com>
2014-06-04 21:17:12 +0800

    新建function工程

---
+++ day_01/01_HelloWorld/01_HelloWorld/Swift_Fun.swift
@@ -0,0 +1,16 @@
+//
+//  Swift_Fun.swift
+//  01_HelloWorld
+//
+//  Created by hc on 14-6-4.
+//  Copyright (c) 2014年 hc. All rights reserved.
+//
+
+import Foundation
+
+
+func printHelloWorld() {
+    println("hello, world")
+}
+
+//摘录来自: Apple Inc. “The Swift Programming Language”。 iBooks. https://itun.es/cn/jEUH0.l
---
+++ day_01/02_Function/02_Function/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  02_Function
+//
+//  Created by hc on 14-6-4.
+//  Copyright (c) 2014年 hc. All rights reserved.
+//
+
+import Foundation
+
+//函数和闭包////////////////////////////////////////////////////////////////////
+//Swift使用func关键字声明函数：
+func greet(name: String, day: String) ->String {
+    return ""
+}
