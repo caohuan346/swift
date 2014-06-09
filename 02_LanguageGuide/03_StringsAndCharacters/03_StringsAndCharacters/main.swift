a5e297846cea9614fceaa1153ba43acbe4ac7542
ch <835965147@qq.com>
2014-06-09 23:58:09 +0800

    新建string工程

---
+++ 02_LanguageGuide/03_StringsAndCharacters/03_StringsAndCharacters/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  03_StringsAndCharacters
+//
+//  Created by hc on 14-6-9.
+//  Copyright (c) 2014年 hc. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
