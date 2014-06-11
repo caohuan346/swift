b848b8e2ef4ed6bfab853a8ea60afca3d3d67b66
hc <835965147@qq.com>
2014-06-11 17:40:36 +0800

    下班commit

--- 02_LanguageGuide/05_Control Flow/05_Control Flow/main.swift
+++ 02_LanguageGuide/05_Control Flow/05_Control Flow/main.swift
@@ -8,5 +8,49 @@
 
 import Foundation
 
-println("Hello, World!")
+// For Loops //////////////////////////////////////////////////////////////////////////////////////////
+
+for i in 1...9 {
+    for j in 1...i {
+        print("\(j)*\(i) = \(i * j) ")
+    }
+    println()
+}
+
+let base = 2
+let power = 5
+var answer = 1
+for _ in 1...power {
+    answer *= base
+}
+
+println("\(base) to the power of \(power) is \(answer)")
+
+
+let names = ["Tom", "Jack", "Rose", "CH"]
+for name in names {
+    print(name + " ")
+}
+
+
+let numberOfLegs = ["spider":8 ,"ant":6, "cat":4]
+
+for (animalName, legCount) in numberOfLegs {
+    println("\(animalName) has \(legCount) legs")
+}
+
+for character in "hello world" {
+    println(character)
+}
+
+
+// For-Condition-Increment /////////////////////////////////////////////////////////////////////
+for var index = 0; index < 3; ++index {
+    println("index is \(index)")
+}
+
+
+
+
+
 
