f7fa4842deb8e068949f9126c15dd57796892797
hc <835965147@qq.com>
2014-06-20 17:38:41 +0800

    some commit

--- 02_LanguageGuide/09_Enumerations/09_Enumerations/main.swift
+++ 02_LanguageGuide/09_Enumerations/09_Enumerations/main.swift
@@ -3,10 +3,81 @@
 //  09_Enumerations
 //
 //  Created by hc on 14-6-16.
-//  Copyright (c) 2014年 hc. All rights reserved.
+//  Copyright (c) 2014 hc. All rights reserved.
 //
 
 import Foundation
+
+//Enumeration Syntax////////////////////////////////////////////////////////////
+enum CompassPoint {
+    case North
+    case South
+    case East
+    case West
+}
+
+enum Planet {
+    case Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
+}
+
+var directionToHead = CompassPoint.East
+println(directionToHead)
+
+//after directionToHead is declared as a CompassPoint ,you can set it with a dot syntax
+directionToHead = .West
+
+//Matching Enumeration Values with a Switch Statement
+directionToHead = .South
+
+switch directionToHead {
+case .East:
+    println("East")
+case .North:
+    println("North")
+case .West:
+    println("West")
+case .South:
+    println("South")
+}
+
+
+let somePlanet = Planet.Earth
+switch somePlanet {
+case .Earth:
+    println("Earth")
+default:
+    println("not Earth")
+}
+
+
+//Associated Values///////////////////////////////////////////////////////////////////////////
+enum Barcode {
+    case UPCA(Int, Int, Int)
+    case QRcode(String)
+}
+
+var aProductBarcode = Barcode.UPCA(8, 85909_51226, 3)
+aProductBarcode = .QRcode("ABCDEFGHIJKLMNOP")
+println(aProductBarcode)
+
+
+//switch aProductBarcode {
+//    case .UPCA
+//    
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
