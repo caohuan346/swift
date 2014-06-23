b80a91bdf6c65ac8e6aa23d4a68cef245277b91a
ch <835965147@qq.com>
2014-06-23 22:47:23 +0800

    1：完成mothod 2：新增工程Subscripts

--- 02_LanguageGuide/12_Methods/12_Methods/main.swift
+++ 02_LanguageGuide/12_Methods/12_Methods/main.swift
@@ -15,6 +15,7 @@ class Counter {
         count++
     }
     
+    
     func incrementBy(amount: Int) {
         count += amount
     }
@@ -158,7 +159,55 @@ class SomeClass {
 SomeClass.someTypeMethod()
 
 ////////////////////////////////////////////////////////////////////////////////
+struct LevelTracker {
+    static var highesUnlockedLevel = 1
+    static func unlockLevel(level: Int) {
+        if level > highesUnlockedLevel {
+            highesUnlockedLevel = level
+        }
+    }
+    
+    static func levelIsUnlocked(level: Int) -> Bool {
+        return level <= highesUnlockedLevel
+    }
+    
+    var currentLevel = 1
+    
+    mutating func advanceToLevel(level: Int) -> Bool {
+        if LevelTracker.levelIsUnlocked(level) {
+            currentLevel = level
+            return true
+        }else {
+            return false
+        }
+    }
+}
 
-////////////////////////////////////////////////////////////////////////////////
+class Player {
+    var tracker = LevelTracker()
+    let playName: String
+    func completedLevel(level: Int) {
+        LevelTracker.unlockLevel(level + 1)
+        tracker.advanceToLevel(level + 1)
+    }
+    
+    init(name: String) {
+        playName = name
+    }
+}
+
+
+var aPlayer = Player(name: "Tom Edison")
+aPlayer.completedLevel(1)
+println("highest unlocked level is now \(LevelTracker.highesUnlockedLevel)")
+//highest unlocked level is now 2
+
+aPlayer = Player(name: "James Bounce")
+if aPlayer.tracker.advanceToLevel(6) {
+    println(" player is now on level 6")
+}else {
+    println("level 6 has not yet been unlocked")
+}
+//level 6 has not yet been unlocked
 
 ////////////////////////////////////////////////////////////////////////////////
---
+++ 02_LanguageGuide/13_Subscripts/13_Subscripts/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  13_Subscripts
+//
+//  Created by hc on 14-6-23.
+//  Copyright (c) 2014 hc. All rights reserved.
+//
+
+import Foundation
+
+
