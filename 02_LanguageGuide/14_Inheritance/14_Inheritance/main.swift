0646284d8d359da9cd9d5d849aa2ffb31bbbfb29
ch <835965147@qq.com>
2014-09-14 23:42:14 +0800

    inherited

--- 02_LanguageGuide/14_Inheritance/14_Inheritance/main.swift
+++ 02_LanguageGuide/14_Inheritance/14_Inheritance/main.swift
@@ -50,19 +50,60 @@ let aTandem = Tandem()
 println(aTandem.numberOfWheels)
 println(aTandem.description())
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class Car: Vehicle {
+    var speed: Double = 0.0
+    init() {
+        super.init()
+        maxPassengers = 5
+        numberOfWheels = 4
+    }
+    
+    override func description() -> String {
+        return super.description() + "; "
+        + "traveling at \(speed) mph"
+    }
+}
 
+var aCar = Car()
+aCar.maxPassengers = 5
+println(aCar.maxPassengers)
+println(aCar.description())
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+class SpeedLimitedCar: Car {
+    override var speed: Double {
+    get {
+        return super.speed;
+    }
+    set {
+        super.speed = min(newValue, 40.0)
+    }
+    }
+}
 
-
+let limitedCar = SpeedLimitedCar()
+limitedCar.speed = 60.0
+println("speed:\(limitedCar.speed)")
+println(limitedCar.description())
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+class AutomaticCar: Car {
+    var gear = 1
+    override var speed: Double {
+    didSet {
+        gear = Int(speed / 10.0) + 1
+    }
+    }
+    
+    override func description() -> String  {
+        return super.description() + "in gear \(gear)"
+    }
+}
 
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
+var automaticCar = AutomaticCar()
+automaticCar.speed = 1000
+println(automaticCar.description())
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
