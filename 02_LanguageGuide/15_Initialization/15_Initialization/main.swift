8547fdcc12ee3d9e2a1948c1fcfca46d1a1d0fbe
Happy <835965147@qq.com>
2014-10-22 18:09:16 +0800

    new

---
+++ 02_LanguageGuide/15_Initialization/15_Initialization/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  15_Initialization
+//
+//  Created by Happy on 14/10/22.
+//  Copyright (c) 2014年 Happy. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
